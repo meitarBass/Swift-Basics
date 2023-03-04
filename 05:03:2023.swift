cb924055811324d753d976bd8a1ca375465d7262
meitarbasson <meitarbass@gmail.com>
2023-03-05 00:24:39 +0200

    First commit

---
+++ 05:03:2023.swift
@@ -0,0 +1,29 @@
+//
+//  05:03:2023.swift
+//  
+//
+//  Created by Meitar Basson on 05/03/2023.
+//
+
+import Foundation
+
+// MARK: Create values
+
+var myVar = 42
+myVar = 50
+
+let varConst = 3.14
+
+
+// MARK: Declare type
+
+let myAge: Int = 3
+let myDoubleAge: Double = 3.5
+
+// MARK: Include values in string
+
+let apples = 3
+
+let sentence = "I got \(apples) in my bag"
+let anotherSentence = "How many " + String(apples)
+
