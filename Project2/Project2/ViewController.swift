d4c8e20b461efff5f09901091d9892f7e25e34b7
meitarbasson <meitarbass@gmail.com>
2023-03-17 02:05:11 +0200

    Need to work on the challenge

--- Project2/Project2/ViewController.swift
+++ Project2/Project2/ViewController.swift
@@ -5,6 +5,19 @@
 //  Created by Meitar Basson on 17/03/2023.
 //
 
+
+/* MARK:
+    CONCEPTS LEARNED WHILE CREATING THIS PROJECT -
+        * @2x, @3x Images
+        * Asset catalog
+        * UIButton
+        * CALayer
+        * UIColor
+        * Random numbers
+        * Actions
+        * UIAlertController
+ */
+
 import UIKit
 
 class ViewController: UIViewController {
@@ -14,21 +27,54 @@ class ViewController: UIViewController {
     @IBOutlet var button3: UIButton!
     
     var countries = [String]()
+    var correctAnswer = 0
     var score = 0
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         
+        // MARK: CALayer, lower level than UIButton, UIColor, etc...
+        
+        button1.layer.borderWidth = 1
+        button2.layer.borderWidth = 1
+        button3.layer.borderWidth = 1
+        
+        button1.layer.borderColor = UIColor.lightGray.cgColor
+        button2.layer.borderColor = UIColor.lightGray.cgColor
+        button3.layer.borderColor = UIColor.lightGray.cgColor
         
         countries += ["estonia", "france", "germany", "ireland", "italy", "monaco", "nigeria", "poland", "russia", "spain", "uk", "us"]
-        askQuestion()
+        askQuestion(action: nil)
     }
     
-    func askQuestion() {
+    func askQuestion(action: UIAlertAction!) {
+        countries.shuffle()
         button1.setImage(UIImage(named: countries[0]), for: .normal)
         button2.setImage(UIImage(named: countries[1]), for: .normal)
         button3.setImage(UIImage(named: countries[2]), for: .normal)
+        
+        correctAnswer = Int.random(in: 0...2)
+        title = countries[correctAnswer].uppercased()
     }
+    
+    @IBAction func buttonTapped(_ sender: UIButton) {
+        var title: String
+        
+        if sender.tag == correctAnswer {
+            title = "Correct"
+            score += 1
+        } else {
+            title = "Wrong, that's the flag of \(countries[sender.tag])"
+            score -= 1
+        }
+        
+        let ac = UIAlertController(title: title, message: "Your score is \(score)", preferredStyle: .alert)
+        ac.addAction(UIAlertAction(title: "Continue", style: .default, handler: askQuestion))
+        present(ac, animated: true)
+    }
+    
+    
+    
 }
 
